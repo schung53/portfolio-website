394509cabf417c4dce2a08b45be53ff2c5c9a64c
James Chung <james.chung@charterup.com>
2023-07-09 17:36:43 -0600

    set up project structure

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,15 @@
 import Head from 'next/head';
 import { AppProps } from 'next/app';
+import '../globals.css';
 
 function App({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <div>
       <Head>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Component {...pageProps} />
-    </>
+    </div>
   );
 }
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,19 @@
+import Image from "next/image";
+import styles from './index.module.css';
+
 function Home() {
-    return <div>Hello world</div>;
+  return (
+    <main className={styles.main}>
+      <div>
+        <Image 
+          src="/name-logo.svg"
+          alt="James S. Chung"
+          width={100}
+          height={100}
+        />
+      </div>
+    </main>
+  );
 }
 
 export default Home;
