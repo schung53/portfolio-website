6d46a230fbbaf1c0088673f08cfb209a8d2f2e21
James Chung <sunhochung53@gmail.com>
2023-07-10 20:11:52 -0600

    style edu items

--- components/education-item.tsx
+++ components/education-item.tsx
@@ -1,26 +1,18 @@
 import styles from './education-item.module.css';
 
-const dummy = {
-  university: "Johns Hopkins University",
-  degreeShorthand: "B.A.",
-  degree: "Bachelor of Arts in Chemistry",
-  date: "2013 - 2017",
-  location: "Baltimore, MD"
-}
-
-function EducationItem() {
+function EducationItem({ item }: any) {
   return (
     <div className={styles.itemContainer}>
       <div className={styles.degreeShorthand}>
-        <p>{dummy.degreeShorthand}</p>
+        <p>{item.degreeShorthand}</p>
       </div>
       <div className={styles.main}>
-        <p className={styles.university}>{dummy.university}</p>
-        <p className={styles.degree}>{dummy.degree}</p>
+        <p className={styles.university}>{item.university}</p>
+        <p className={styles.degree}>{item.degree}</p>
         <div className={styles.infoLine}>
-          <p>{dummy.date}</p>
+          <p>{item.date}</p>
           <div className={styles.dashedLine} />
-          <p>{dummy.location}</p>
+          <p>{item.location}</p>
         </div>
       </div>
     </div>
--- components/education-tab.tsx
+++ components/education-tab.tsx
@@ -1,10 +1,11 @@
 import EducationItem from './education-item';
 import styles from './education-tab.module.css';
+import data from '../data/education.json'
 
 function EducationTab() {
   return (
     <div className={styles.container}>
-      <EducationItem />
+      {data.map((item) => <EducationItem key={item.degree} item={item} />)}
     </div>
   );
 }
