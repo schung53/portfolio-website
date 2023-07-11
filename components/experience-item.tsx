c7e3a00f8381262c6384df66d6a327d150790774
James Chung <sunhochung53@gmail.com>
2023-07-10 23:34:55 -0600

    formatting

--- components/experience-item.tsx
+++ components/experience-item.tsx
@@ -3,8 +3,13 @@ import styles from './experience-item.module.css'
 function ExperienceItem({ item }: any) {
   return (
     <div className={styles.itemContainer}>
-      <span>
+      <span className={styles.titleLine}>
         <p className={styles.title}>{`${item.title} • ${item.company}`}</p>
+      </span>
+      <span className={styles.infoLine}>
+        <p>{item.date}</p>
+        <div className={styles.dashedLine} />
+        <p>{item.location}</p>
       </span>
     </div>
   );
