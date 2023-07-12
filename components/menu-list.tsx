ecfead211a5af85d441764d77fa88c391f54fbee
James Chung <sunhochung53@gmail.com>
2023-07-12 17:25:21 -0600

    implement disappearing list

--- components/menu-list.tsx
+++ components/menu-list.tsx
@@ -1,21 +1,32 @@
+import { useEffect, useState } from 'react';
 import PropTypes, { InferProps } from 'prop-types';
 import styles from './menu-list.module.css';
 import Slide from '@mui/material/Slide';
 
 function MenuList({ onClick, isMenuOpen }: InferProps<typeof MenuList.propTypes>) {
+  const [showList, setShowList] = useState(true);
+
+  useEffect(() => {
+    setShowList(isMenuOpen!);
+  }, [isMenuOpen]);
+
   const handleClick = (tabId: number) => {
+    setShowList(false);
     onClick?.(tabId);
   }
-  
+
   return (
     <Slide direction="right" in={isMenuOpen!} mountOnEnter unmountOnExit>
-      <div id="menu-list" className={styles.listContainer}>
-        <div className={styles.listItem} onClick={() => handleClick(0)}>Home</div>
-        <div className={styles.listItem} onClick={() => handleClick(1)}>Experience</div>
-        <div className={styles.listItem} onClick={() => handleClick(2)}>Education</div>
-        <div className={styles.listItem} onClick={() => handleClick(3)}>Projects</div>
-        <div className={styles.listItem} onClick={() => handleClick(4)}>Resume</div>
-      </div>
+      <div>
+        {showList && 
+          <div id="menu-list" className={styles.listContainer}>
+            <div className={styles.listItem} onClick={() => handleClick(0)}>Home</div>
+            <div className={styles.listItem} onClick={() => handleClick(1)}>Experience</div>
+            <div className={styles.listItem} onClick={() => handleClick(2)}>Education</div>
+            <div className={styles.listItem} onClick={() => handleClick(3)}>Projects</div>
+            <div className={styles.listItem} onClick={() => handleClick(4)}>Resume</div>
+          </div>}
+        </div>
     </Slide>
   );
 }
