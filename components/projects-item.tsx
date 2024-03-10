8bd6be800e7ed9f9775716db564bf2ce13ae012d
James Chung <sunhochung53@gmail.com>
2024-03-10 00:45:05 -0800

    Add projects layout

--- components/projects-item.tsx
+++ components/projects-item.tsx
@@ -1,15 +1,98 @@
-import { useEffect, useState } from 'react';
-import Image from 'next/image';
-import styles from './education-item.module.css';
-import { Fade, Grid } from '@mui/material';
-import { MediaType } from './enum';
-import { WHITE_TEXT_THEMES } from '@/utils/color';
+import { useEffect, useState } from "react";
+import Image from "next/image";
+import styles from "./projects-item.module.css";
+import { WHITE_TEXT_THEMES } from "@/utils/color";
+import { MediaType } from "./enum";
 
 function ProjectsItem({ item, mediaType, color }: any) {
-  const [textColor, setTextColor] = useState("#000000")
+  const [textColor, setTextColor] = useState("#000000");
+  const [holePunchDiameter, setHolePunchDiameter] = useState("18vh");
+
+  useEffect(() => {
+    if (WHITE_TEXT_THEMES.includes(color)) {
+      setTextColor("#FADFB5");
+    } else {
+      setTextColor("#000000");
+    }
+  }, [color]);
+
+  useEffect(() => {
+    if (mediaType === MediaType.Mobile) {
+      setHolePunchDiameter("12vh");
+    } else if (mediaType === MediaType.Tablet) {
+      setHolePunchDiameter("14vh");
+    } else {
+      setHolePunchDiameter("18vh");
+    }
+  }, [mediaType]);
+
+  const validateDescription = (description: string) => {
+    if (description.includes("Nature")) {
+      const splitDescription = description.split("Nature");
+      return (
+        <>
+          {splitDescription[0]}
+          <i>Nature</i>
+          {splitDescription[1]}
+        </>
+      );
+    }
+    return description;
+  };
+
+  const adjust = (dimension: number) => {
+    if (mediaType === MediaType.Mobile) {
+      return dimension * 0.7;
+    } else if (mediaType === MediaType.Tablet) {
+      return dimension * 0.9;
+    }
+    return dimension;
+  };
 
   return (
-    <div className={styles.itemContainer} style={{ backgroundColor: color, color: textColor }}></div>
+    <div
+      className={styles.itemContainer}
+      style={{ backgroundColor: color, color: textColor }}
+    >
+      <div style={{ display: "flex", alignItems: "center" }}>
+        <div
+          className={styles.holePunch}
+          style={{ width: holePunchDiameter, height: holePunchDiameter }}
+        >
+          <Image
+            src={item.image}
+            alt="Project Logo"
+            width={adjust(item.dimensions[0])}
+            height={adjust(item.dimensions[1])}
+          />
+        </div>
+        {mediaType === MediaType.Mobile && (
+          <div className={styles.mobileTitleContainer}>
+            <span>
+              <p className={styles.title}>{item.title}</p>
+            </span>
+            <span>
+              <p className={styles.subtitle}>{`${item.date} • ${item.type}`}</p>
+            </span>
+          </div>
+        )}
+      </div>
+      <div className={styles.textContainer}>
+        {mediaType !== MediaType.Mobile && (
+          <>
+            <span>
+              <p className={styles.title}>{item.title}</p>
+            </span>
+            <span>
+              <p className={styles.subtitle}>{`${item.date} • ${item.type}`}</p>
+            </span>
+          </>
+        )}
+        <p className={styles.description}>
+          {validateDescription(item.description)}
+        </p>
+      </div>
+    </div>
   );
 }
 
